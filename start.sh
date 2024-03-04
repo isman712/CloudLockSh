421009dd73f404d86dcb6561617e79d6485174f7
Isman712 <derekisman712@gmail.com>
2024-03-04 11:32:39 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,31 @@
 echo "                                                                                                                  
 
-
- ▄████████  ▄█        ▄██████▄  ███    █▄  ████████▄   ▄█        ▄██████▄   ▄████████    ▄█   ▄█▄      
-███    ███ ███       ███    ███ ███    ███ ███   ▀███ ███       ███    ███ ███    ███   ███ ▄███▀      
-███    █▀  ███       ███    ███ ███    ███ ███    ███ ███       ███    ███ ███    █▀    ███▐██▀        
-███        ███       ███    ███ ███    ███ ███    ███ ███       ███    ███ ███         ▄█████▀         
-███        ███       ███    ███ ███    ███ ███    ███ ███       ███    ███ ███        ▀▀█████▄         
-███    █▄  ███       ███    ███ ███    ███ ███    ███ ███       ███    ███ ███    █▄    ███▐██▄        
-███    ███ ███▌    ▄ ███    ███ ███    ███ ███   ▄███ ███▌    ▄ ███    ███ ███    ███   ███ ▀███▄      
-████████▀  █████▄▄██  ▀██████▀  ████████▀  ████████▀  █████▄▄██  ▀██████▀  ████████▀    ███   ▀█▀      
-           ▀                                          ▀                                 ▀              
-                                                                                    
-                                                                                                                                                                          
+                                                                                                                                                                         
+                                                                            dddddddd                                                                                     
+        CCCCCCCCCCCCClllllll                                                d::::::dLLLLLLLLLLL                                                  kkkkkkkk                
+     CCC::::::::::::Cl:::::l                                                d::::::dL:::::::::L                                                  k::::::k                
+   CC:::::::::::::::Cl:::::l                                                d::::::dL:::::::::L                                                  k::::::k                
+  C:::::CCCCCCCC::::Cl:::::l                                                d:::::d LL:::::::LL                                                  k::::::k                
+ C:::::C       CCCCCC l::::l    ooooooooooo   uuuuuu    uuuuuu      ddddddddd:::::d   L:::::L                  ooooooooooo       cccccccccccccccc k:::::k    kkkkkkk     
+C:::::C               l::::l  oo:::::::::::oo u::::u    u::::u    dd::::::::::::::d   L:::::L                oo:::::::::::oo   cc:::::::::::::::c k:::::k   k:::::k      
+C:::::C               l::::l o:::::::::::::::ou::::u    u::::u   d::::::::::::::::d   L:::::L               o:::::::::::::::o c:::::::::::::::::c k:::::k  k:::::k       
+C:::::C               l::::l o:::::ooooo:::::ou::::u    u::::u  d:::::::ddddd:::::d   L:::::L               o:::::ooooo:::::oc:::::::cccccc:::::c k:::::k k:::::k        
+C:::::C               l::::l o::::o     o::::ou::::u    u::::u  d::::::d    d:::::d   L:::::L               o::::o     o::::oc::::::c     ccccccc k::::::k:::::k         
+C:::::C               l::::l o::::o     o::::ou::::u    u::::u  d:::::d     d:::::d   L:::::L               o::::o     o::::oc:::::c              k:::::::::::k          
+C:::::C               l::::l o::::o     o::::ou::::u    u::::u  d:::::d     d:::::d   L:::::L               o::::o     o::::oc:::::c              k:::::::::::k          
+ C:::::C       CCCCCC l::::l o::::o     o::::ou:::::uuuu:::::u  d:::::d     d:::::d   L:::::L         LLLLLLo::::o     o::::oc::::::c     ccccccc k::::::k:::::k         
+  C:::::CCCCCCCC::::Cl::::::lo:::::ooooo:::::ou:::::::::::::::uud::::::ddddd::::::ddLL:::::::LLLLLLLLL:::::Lo:::::ooooo:::::oc:::::::cccccc:::::ck::::::k k:::::k        
+   CC:::::::::::::::Cl::::::lo:::::::::::::::o u:::::::::::::::u d:::::::::::::::::dL::::::::::::::::::::::Lo:::::::::::::::o c:::::::::::::::::ck::::::k  k:::::k       
+     CCC::::::::::::Cl::::::l oo:::::::::::oo   uu::::::::uu:::u  d:::::::::ddd::::dL::::::::::::::::::::::L oo:::::::::::oo   cc:::::::::::::::ck::::::k   k:::::k      
+        CCCCCCCCCCCCCllllllll   ooooooooooo       uuuuuuuu  uuuu   ddddddddd   dddddLLLLLLLLLLLLLLLLLLLLLLLL   ooooooooooo       cccccccccccccccckkkkkkkk    kkkkkkk     
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                         
+                                                                                                                                                                      
 "
 
 echo "Mensaje de CloudLock en ASCII Art"
