d6c187b19c16e8250cc748ae8088430f2f7a80e8
Isman712 <derekisman712@gmail.com>
2024-03-04 11:21:18 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,21 @@
-#!/bin/bash
+echo "                                                                                                                  
+  CCCCCCCCCCCC   lllllll lllllll                                                                                       
+CCC::::::::::::C  l:::::l l:::::l                                                                                       
+CC:::::::::::::::C l:::::l l:::::l                                                                                       
+C:::::CCCCCCCC::::Cl:::::l l:::::l                                                                                       
+C:::::C       CCCCCC l::::l  l::::l  ooooooooooo      uuuuuu    uuuuuu rrrrr   rrrrrrrrr       ssssssssss       
+C:::::C                l::::l  l::::l oo:::::::::::oo   u::::u    u::::u r::::rrr:::::::::r    ss::::::::::s      
+C:::::C                l::::l  l::::l o:::::::::::::::o u::::u    u::::u r:::::::::::::::::r ss:::::::::::::s     
+C:::::C                l::::l  l::::l o:::::ooooo:::::o u::::u    u::::u rr::::::rrrrr::::::rs::::::ssss:::::s    
+C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r     r:::::r s:::::s  ssssss     
+C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r     rrrrrrr   s::::::s          
+C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r                  s::::::s       
+ C:::::C       CCCCCC l::::::ll::::::lo::::o     o::::o u::::::uuuu:::::u  r:::::r            ssssss   s:::::s     
+  C:::::CCCCCCCC::::Cl::::::ll::::::lo:::::ooooo:::::o  u:::::::::::::::uur:::::r            s:::::ssss::::::s    
+   CC:::::::::::::::Cl::::::ll::::::lo:::::::::::::::o   u:::::::::::::::ur:::::r            s::::::::::::::s     
+     CCC::::::::::::Cl::::::ll::::::l oo:::::::::::oo     uu::::::::uu:::ur:::::r             s:::::::::::ss      
+        CCCCCCCCCCCCllllllllllllllll   ooooooooooo         uuuuuuuu  uuuurrrrrrr              sssssssssss       
+                                                                                                                  
+"
 
-# Este script muestra un mensaje en pantalla enorme utilizando toilet
-
-# Instalar toilet si no está instalado
-if ! command -v toilet &> /dev/null; then
-    echo "Instalando toilet..."
-    # Comando para instalar toilet, puede variar dependiendo del sistema operativo
-    # Por ejemplo, en Debian/Ubuntu sería: sudo apt-get install toilet
-    # En CentOS/RHEL: sudo yum install toilet
-    # Asegúrate de tener permisos de administrador para instalar el paquete
-    exit 1
-fi
-
-# Mostrar el mensaje en pantalla
-toilet -f future -F metal "CloudLock"
+echo "Mensaje de CloudLock en ASCII Art"
