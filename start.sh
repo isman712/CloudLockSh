8d28267d8c04ffc4104e983241990e8e7ea71283
Isman712 <derekisman712@gmail.com>
2024-03-04 11:24:07 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,12 @@
 echo "                                                                                                                  
-  CCCCCCCCCCCC   lllllll lllllll                                                                                       
-CCC::::::::::::C  l:::::l l:::::l                                                                                       
-CC:::::::::::::::C l:::::l l:::::l                                                                                       
-C:::::CCCCCCCC::::Cl:::::l l:::::l                                                                                       
-C:::::C       CCCCCC l::::l  l::::l  ooooooooooo      uuuuuu    uuuuuu rrrrr   rrrrrrrrr       ssssssssss       
-C:::::C                l::::l  l::::l oo:::::::::::oo   u::::u    u::::u r::::rrr:::::::::r    ss::::::::::s      
-C:::::C                l::::l  l::::l o:::::::::::::::o u::::u    u::::u r:::::::::::::::::r ss:::::::::::::s     
-C:::::C                l::::l  l::::l o:::::ooooo:::::o u::::u    u::::u rr::::::rrrrr::::::rs::::::ssss:::::s    
-C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r     r:::::r s:::::s  ssssss     
-C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r     rrrrrrr   s::::::s          
-C:::::C                l::::l  l::::l o::::o     o::::o u::::u    u::::u  r:::::r                  s::::::s       
- C:::::C       CCCCCC l::::::ll::::::lo::::o     o::::o u::::::uuuu:::::u  r:::::r            ssssss   s:::::s     
-  C:::::CCCCCCCC::::Cl::::::ll::::::lo:::::ooooo:::::o  u:::::::::::::::uur:::::r            s:::::ssss::::::s    
-   CC:::::::::::::::Cl::::::ll::::::lo:::::::::::::::o   u:::::::::::::::ur:::::r            s::::::::::::::s     
-     CCC::::::::::::Cl::::::ll::::::l oo:::::::::::oo     uu::::::::uu:::ur:::::r             s:::::::::::ss      
-        CCCCCCCCCCCCllllllllllllllll   ooooooooooo         uuuuuuuu  uuuurrrrrrr              sssssssssss       
-                                                                                                                  
+
+   ______ __                   __ __                 __      
+  / ____// /____   __  __ ____/ // /   ____   _____ / /__    
+ / /    / // __ \ / / / // __  // /   / __ \ / ___// //_/    
+/ /___ / // /_/ // /_/ // /_/ // /___/ /_/ // /__ / ,<       
+\____//_/ \____/ \__,_/ \__,_//_____/\____/ \___//_/|_|      
+                                                             
+                                                                                                          
 "
 
 echo "Mensaje de CloudLock en ASCII Art"
